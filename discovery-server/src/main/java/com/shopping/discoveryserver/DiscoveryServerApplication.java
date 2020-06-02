03fb4d474213444dae23b9b1b55ab52a9784aae2
ahmsay <ahmsay@gmail.com>
2020-06-02 19:13:07 +0300

    discovery server added

---
+++ discovery-server/src/main/java/com/shopping/discoveryserver/DiscoveryServerApplication.java
@@ -0,0 +1,13 @@
+package com.shopping.discoveryserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiscoveryServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiscoveryServerApplication.class, args);
+	}
+
+}
