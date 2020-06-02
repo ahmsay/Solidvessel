96f3a40dd8a073d40e946984e545990ac502a9d9
ahmsay <ahmsay@gmail.com>
2020-06-02 20:01:01 +0300

    services configured for discovery discovery server

--- account-service/src/main/java/com/shopping/accountservice/AccountServiceApplication.java
+++ account-service/src/main/java/com/shopping/accountservice/AccountServiceApplication.java
@@ -2,10 +2,12 @@ package com.shopping.accountservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class AccountServiceApplication {
 
 	@Bean
