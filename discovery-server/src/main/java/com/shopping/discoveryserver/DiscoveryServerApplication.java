20ec615a9287d717c1017300744704e02ace4207
ahmsay <ahmsay@gmail.com>
2020-06-02 19:25:41 +0300

    discovery server configured to run properly

--- discovery-server/src/main/java/com/shopping/discoveryserver/DiscoveryServerApplication.java
+++ discovery-server/src/main/java/com/shopping/discoveryserver/DiscoveryServerApplication.java
@@ -2,8 +2,10 @@ package com.shopping.discoveryserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class DiscoveryServerApplication {
 
 	public static void main(String[] args) {
