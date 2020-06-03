241588249377bc89eec1582891dbcdbf84b8b868
ahmsay <ahmsay@gmail.com>
2020-06-03 19:12:28 +0300

    loadbalanced is added to rest template

--- account-service/src/main/java/com/shopping/accountservice/AccountServiceApplication.java
+++ account-service/src/main/java/com/shopping/accountservice/AccountServiceApplication.java
@@ -2,6 +2,7 @@ package com.shopping.accountservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
@@ -11,6 +12,7 @@ import org.springframework.web.client.RestTemplate;
 public class AccountServiceApplication {
 
 	@Bean
+	@LoadBalanced
 	public RestTemplate getRestTemplate() {
 		return new RestTemplate();
 	}
