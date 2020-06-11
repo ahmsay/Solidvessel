96336940f65105111f7b8b9a994ef76886c9024a
ahmsay <ahmsay@gmail.com>
2020-06-11 21:45:59 +0300

    spring cloud config server added

---
+++ config-server/src/main/java/com/shopping/configserver/ConfigurationServerApplication.java
@@ -0,0 +1,13 @@
+package com.shopping.configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigurationServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigurationServerApplication.class, args);
+	}
+
+}
