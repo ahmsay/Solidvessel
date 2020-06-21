ffe77c7a937321b0edc633cf254a52a93596fb58
ahmsay <ahmsay@gmail.com>
2020-06-21 11:26:18 +0300

    webclient added

--- payment-service/src/main/java/com/shopping/paymentservice/PaymentServiceApplication.java
+++ payment-service/src/main/java/com/shopping/paymentservice/PaymentServiceApplication.java
@@ -6,6 +6,7 @@ import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @SpringBootApplication
 @EnableEurekaClient
@@ -15,6 +16,12 @@ public class PaymentServiceApplication {
 	@LoadBalanced
 	public RestTemplate getRestTemplate() {
 		return new RestTemplate();
+	}
+
+	@Bean
+	@LoadBalanced
+	public WebClient.Builder getWebClientBuilder() {
+		return WebClient.builder();
 	}
 
 	public static void main(String[] args) {
