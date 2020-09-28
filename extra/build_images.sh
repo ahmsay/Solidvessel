aa1a90c77077667c579a2112ab2a9b2d780aee2d
ahmsay <ahmsay@gmail.com>
2020-09-28 23:23:00 +0300

    build script is added

---
+++ extra/build_images.sh
@@ -0,0 +1,30 @@
+cd account-service
+mvn clean package
+docker build --tag ahmsay/online-shopping-application:account-service .
+cd ..
+
+cd inventory-service
+mvn clean package
+docker build --tag ahmsay/online-shopping-application:inventory-service .
+cd ..
+
+cd payment-service
+mvn clean package
+docker build --tag ahmsay/online-shopping-application:payment-service .
+cd ..
+
+cd order-service
+mvn clean package
+docker build --tag ahmsay/online-shopping-application:order-service .
+cd ..
+
+cd discovery-server
+mvn clean package
+docker build --tag ahmsay/online-shopping-application:discovery-server .
+cd ..
+
+cd config-server
+mvn clean package
+docker build --tag ahmsay/online-shopping-application:config-server .
+cd ..
+
