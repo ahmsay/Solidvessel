98430aaad53938d5afa60ae96aa7c741bb1b32ee
ahmsay <ahmsay@gmail.com>
2020-12-11 20:59:44 +0300

    unnecessary requests are removed

--- order-service/src/main/java/com/microshop/orderservice/controllers/OrderRestController.java
+++ order-service/src/main/java/com/microshop/orderservice/controllers/OrderRestController.java
@@ -1,25 +1,17 @@
 package com.microshop.orderservice.controllers;
 
 import com.microshop.orderservice.dto.OrderDTO;
-import com.microshop.orderservice.entity.Customer;
 import com.microshop.orderservice.entity.Order;
-import com.microshop.orderservice.entity.Payment;
-import com.microshop.orderservice.services.ICustomerService;
 import com.microshop.orderservice.services.IOrderService;
-import com.microshop.orderservice.services.IPaymentService;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class OrderRestController {
 
     private final IOrderService orderService;
-    private final ICustomerService customerService;
-    private final IPaymentService paymentService;
 
-    public OrderRestController(final IOrderService orderService, final ICustomerService customerService, final IPaymentService paymentService) {
+    public OrderRestController(final IOrderService orderService) {
         this.orderService = orderService;
-        this.customerService = customerService;
-        this.paymentService = paymentService;
     }
 
     @GetMapping("/orders")
@@ -35,16 +27,6 @@ public class OrderRestController {
     @GetMapping("/orders/ofCustomer/{customerId}")
     public Iterable<Order> findByCustomerId(@PathVariable final Long customerId) {
         return orderService.findByCustomerId(customerId);
-    }
-
-    @GetMapping("/orders/{id}/customer")
-    public Customer findCustomerOfOrder(@PathVariable final Long id) {
-        return customerService.findById(id);
-    }
-
-    @GetMapping("/orders/{id}/payment")
-    public Payment findPaymentOfOrder(@PathVariable final Long id) {
-        return paymentService.findById(id);
     }
 
     @PostMapping("/orders")
