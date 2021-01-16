abdf883a2bca41dd4817c1e0b3a7ef1a049f73a7
ahmsay <ahmsay@gmail.com>
2021-01-16 12:09:03 +0300

    rabbitmq is configured for payment service

---
+++ payment-service/src/main/java/com/microshop/paymentservice/configuration/RabbiqMQ.java
@@ -0,0 +1,30 @@
+package com.microshop.paymentservice.configuration;
+
+import org.springframework.amqp.core.TopicExchange;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbiqMQ {
+
+    @Bean
+    public TopicExchange topicExchange(@Value("${payment.exchange}") final String exchangeName) {
+        return new TopicExchange(exchangeName);
+    }
+
+    @Bean
+    public RabbitTemplate rabbitTemplate(final ConnectionFactory connectionFactory) {
+        final RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+        rabbitTemplate.setMessageConverter(producerJackson2MessageConverter());
+        return rabbitTemplate;
+    }
+
+    @Bean
+    public Jackson2JsonMessageConverter producerJackson2MessageConverter() {
+        return new Jackson2JsonMessageConverter();
+    }
+}
