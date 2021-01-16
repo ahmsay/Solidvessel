bec846808505f415167ac07949d4cfaaf72f0918
ahmsay <ahmsay@gmail.com>
2021-01-16 13:12:09 +0300

    rabbitmq is configured for inventory service

---
+++ inventory-service/src/main/java/com/microshop/inventoryservice/configuration/RabbitMQ.java
@@ -0,0 +1,50 @@
+package com.microshop.inventoryservice.configuration;
+
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.TopicExchange;
+import org.springframework.amqp.rabbit.annotation.RabbitListenerConfigurer;
+import org.springframework.amqp.rabbit.listener.RabbitListenerEndpointRegistrar;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.converter.MappingJackson2MessageConverter;
+import org.springframework.messaging.handler.annotation.support.DefaultMessageHandlerMethodFactory;
+
+@Configuration
+public class RabbitMQ implements RabbitListenerConfigurer {
+
+    @Bean
+    public TopicExchange topicExchange(@Value("${payment.exchange}") final String exchangeName) {
+        return new TopicExchange(exchangeName);
+    }
+
+    @Bean
+    public Queue queue(@Value("${product.queue}") final String queueName) {
+        return new Queue(queueName, true);
+    }
+
+    @Bean
+    Binding binding(final Queue queue, final TopicExchange exchange,
+                    @Value("${payment.saved.key}") final String routingKey) {
+        return BindingBuilder.bind(queue).to(exchange).with(routingKey);
+    }
+
+    @Override
+    public void configureRabbitListeners(final RabbitListenerEndpointRegistrar registrar) {
+        registrar.setMessageHandlerMethodFactory(messageHandlerMethodFactory());
+    }
+
+    @Bean
+    public DefaultMessageHandlerMethodFactory messageHandlerMethodFactory() {
+        DefaultMessageHandlerMethodFactory factory = new DefaultMessageHandlerMethodFactory();
+        factory.setMessageConverter(consumerJackson2MessageConverter());
+        return factory;
+    }
+
+    @Bean
+    public MappingJackson2MessageConverter consumerJackson2MessageConverter() {
+        return new MappingJackson2MessageConverter();
+    }
+}
