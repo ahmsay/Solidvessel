593fe86cb4b69e00b1df97db2bfeb1d8d673b578
ahmsay <ahmsay@gmail.com>
2021-01-19 20:14:33 +0300

    build images file is updated according to new docker repository structure

--- extra/build_images.sh
+++ extra/build_images.sh
@@ -2,31 +2,30 @@ cd ..
 
 cd account-service
 mvn clean package
-docker build --tag ahmsay/microshop:account-service .
+docker build --tag ahmsay/microshop-account-service:1.0 .
 cd ..
 
 cd inventory-service
 mvn clean package
-docker build --tag ahmsay/microshop:inventory-service .
+docker build --tag ahmsay/microshop-inventory-service:1.0 .
 cd ..
 
 cd payment-service
 mvn clean package
-docker build --tag ahmsay/microshop:payment-service .
+docker build --tag ahmsay/microshop-payment-service:1.0 .
 cd ..
 
 cd order-service
 mvn clean package
-docker build --tag ahmsay/microshop:order-service .
+docker build --tag ahmsay/microshop-order-service:1.0 .
 cd ..
 
 cd discovery-server
 mvn clean package
-docker build --tag ahmsay/microshop:discovery-server .
+docker build --tag ahmsay/microshop-discovery-server:1.0 .
 cd ..
 
 cd config-server
 mvn clean package
-docker build --tag ahmsay/microshop:config-server .
+docker build --tag ahmsay/microshop-config-server:1.0 .
 cd ..
-
