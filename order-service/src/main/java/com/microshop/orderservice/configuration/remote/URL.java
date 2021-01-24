fe5dd790bffd7ac171bf7f300f6b7b9e4a407f60
ahmsay <ahmsay@gmail.com>
2021-01-24 11:12:37 +0300

    service urls will be taken from configuration classes

---
+++ order-service/src/main/java/com/microshop/orderservice/configuration/remote/URL.java
@@ -0,0 +1,48 @@
+package com.microshop.orderservice.configuration.remote;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.context.annotation.Configuration;
+
+@RefreshScope
+@Configuration
+@ConfigurationProperties
+public class URL {
+
+    private String accountServiceUrl;
+    private String inventoryServiceUrl;
+    private String orderServiceUrl;
+    private String paymentServiceUrl;
+
+    public String getAccountServiceUrl() {
+        return accountServiceUrl;
+    }
+
+    public void setAccountServiceUrl(final String accountServiceUrl) {
+        this.accountServiceUrl = accountServiceUrl;
+    }
+
+    public String getInventoryServiceUrl() {
+        return inventoryServiceUrl;
+    }
+
+    public void setInventoryServiceUrl(final String inventoryServiceUrl) {
+        this.inventoryServiceUrl = inventoryServiceUrl;
+    }
+
+    public String getOrderServiceUrl() {
+        return orderServiceUrl;
+    }
+
+    public void setOrderServiceUrl(final String orderServiceUrl) {
+        this.orderServiceUrl = orderServiceUrl;
+    }
+
+    public String getPaymentServiceUrl() {
+        return paymentServiceUrl;
+    }
+
+    public void setPaymentServiceUrl(final String paymentServiceUrl) {
+        this.paymentServiceUrl = paymentServiceUrl;
+    }
+}
