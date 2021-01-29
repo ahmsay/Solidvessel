b40d312c30d96aee34c2b0d9d04fd833ea8d855c
ahmsay <ahmsay@gmail.com>
2021-01-29 22:52:42 +0300

    docker image tag names will be latest

--- extra/build_images.sh
+++ extra/build_images.sh
@@ -2,30 +2,30 @@ cd ..
 
 cd account-service
 mvn clean package
-docker build --tag ahmsay/microshop-account-service:1.0 .
+docker build --tag ahmsay/microshop-account-service:latest .
 cd ..
 
 cd inventory-service
 mvn clean package
-docker build --tag ahmsay/microshop-inventory-service:1.0 .
+docker build --tag ahmsay/microshop-inventory-service:latest .
 cd ..
 
 cd payment-service
 mvn clean package
-docker build --tag ahmsay/microshop-payment-service:1.0 .
+docker build --tag ahmsay/microshop-payment-service:latest .
 cd ..
 
 cd order-service
 mvn clean package
-docker build --tag ahmsay/microshop-order-service:1.0 .
+docker build --tag ahmsay/microshop-order-service:latest .
 cd ..
 
 cd discovery-server
 mvn clean package
-docker build --tag ahmsay/microshop-discovery-server:1.0 .
+docker build --tag ahmsay/microshop-discovery-server:latest .
 cd ..
 
 cd config-server
 mvn clean package
-docker build --tag ahmsay/microshop-config-server:1.0 .
+docker build --tag ahmsay/microshop-config-server:latest .
 cd ..
