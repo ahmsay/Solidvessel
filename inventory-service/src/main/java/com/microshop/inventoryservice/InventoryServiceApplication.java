e0f6d7b9a7ec7441263abf06d32c67be422ae6aa
ahmsay <ahmsay@gmail.com>
2021-02-04 19:39:56 +0300

    config server integration and remote package may be removed in this microservice

--- inventory-service/src/main/java/com/microshop/inventoryservice/InventoryServiceApplication.java
+++ inventory-service/src/main/java/com/microshop/inventoryservice/InventoryServiceApplication.java
@@ -7,7 +7,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 @SpringBootApplication
 @EnableEurekaClient
 public class InventoryServiceApplication {
-
+	// TODO Config server integration and remote package may be removed in this microservice.
 	public static void main(final String[] args) {
 		SpringApplication.run(InventoryServiceApplication.class, args);
 	}
