08dffb3452df9e38c6e4d9c7f51d415837053856
ahmsay <ahmsay@gmail.com>
2021-02-06 22:14:00 +0300

    entity and identifier is fixed

--- payment-service/src/main/java/com/microshop/paymentservice/repositories/IPaymentProductRepository.java
+++ payment-service/src/main/java/com/microshop/paymentservice/repositories/IPaymentProductRepository.java
@@ -1,7 +1,9 @@
 package com.microshop.paymentservice.repositories;
 
+import com.microshop.paymentservice.entity.PaymentProduct;
+import com.microshop.paymentservice.entity.PaymentProductId;
 import org.springframework.data.repository.CrudRepository;
 
-public interface IPaymentProductRepository extends CrudRepository<Long, Long> {
+public interface IPaymentProductRepository extends CrudRepository<PaymentProduct, PaymentProductId> {
 
 }
