a312068a3cee93d6ce898bf4a35255a64e122cfa
ahmsay <ahmsay@gmail.com>
2021-02-11 20:36:49 +0300

    zuul proxy is enabled and routes are added

--- gateway-service/src/main/java/com/microshop/gatewayservice/GatewayServiceApplication.java
+++ gateway-service/src/main/java/com/microshop/gatewayservice/GatewayServiceApplication.java
@@ -2,12 +2,13 @@ package com.microshop.gatewayservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
+@EnableZuulProxy
 @SpringBootApplication
 public class GatewayServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(GatewayServiceApplication.class, args);
     }
-
 }
