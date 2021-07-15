4064ae36a432ffc108089cf596d20d6f9ed204a4
ahmsay <ahmsay@gmail.com>
2021-07-15 23:51:43 +0300

    spring cloud gateway is incompatible with spring web. spring config is removed as well

--- gateway-service/src/main/java/com/microshop/gatewayservice/GatewayServiceApplication.java
+++ gateway-service/src/main/java/com/microshop/gatewayservice/GatewayServiceApplication.java
@@ -3,9 +3,7 @@ package com.microshop.gatewayservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
-@EnableZuulProxy
 @EnableEurekaClient
 @SpringBootApplication
 public class GatewayServiceApplication {
