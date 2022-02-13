900a8f69bbc862dc31a9c33d0dfb6feec2ce4fd5
ahmsay <ahmsay@gmail.com>
2022-02-13 12:21:19 +0300

    unrecognized fields are ignored in events

--- inventory-service/src/main/java/com/microshop/inventoryservice/event/PaymentSavedEvent.java
+++ inventory-service/src/main/java/com/microshop/inventoryservice/event/PaymentSavedEvent.java
@@ -1,6 +1,9 @@
 package com.microshop.inventoryservice.event;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import java.util.List;
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 public record PaymentSavedEvent(Long paymentId, List<Long> productIds) {
 }
--- order-service/src/main/java/com/microshop/orderservice/event/PaymentSavedEvent.java
+++ order-service/src/main/java/com/microshop/orderservice/event/PaymentSavedEvent.java
@@ -1,4 +1,7 @@
 package com.microshop.orderservice.event;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
 public record PaymentSavedEvent(Long paymentId, Long customerId) {
 }
