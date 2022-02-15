2c8d1d895c3feb08d2aefadc0dc16f027e6f250e
ahmsay <ahmsay@gmail.com>
2022-02-15 21:01:25 +0300

    k8s test env configuration is created

---
+++ project-configuration/kubernetes/test/start.sh
@@ -0,0 +1,38 @@
+kubectl create ns microshop
+
+cd rabbitmq
+kubectl create -f pod.yaml
+kubectl create -f service.yaml
+cd ..
+
+cd account/db
+kubectl create -f pod.yaml
+kubectl create -f service.yaml
+cd ../app
+kubectl create -f deployment.yaml
+kubectl create -f service.yaml
+cd ../..
+
+cd inventory/db
+kubectl create -f pod.yaml
+kubectl create -f service.yaml
+cd ../app
+kubectl create -f deployment.yaml
+kubectl create -f service.yaml
+cd ../..
+
+cd order/db
+kubectl create -f pod.yaml
+kubectl create -f service.yaml
+cd ../app
+kubectl create -f deployment.yaml
+kubectl create -f service.yaml
+cd ../..
+
+cd payment/db
+kubectl create -f pod.yaml
+kubectl create -f service.yaml
+cd ../app
+kubectl create -f deployment.yaml
+kubectl create -f service.yaml
+cd ../..
