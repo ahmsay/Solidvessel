1045952cf6005d66cfbad5bb06850a8f64848f66
ahmsay <ahmsay@gmail.com>
2022-02-24 20:53:06 +0300

    auth service is added

---
+++ auth/src/test/java/com/microshop/auth/AuthApplicationTests.java
@@ -0,0 +1,12 @@
+package com.microshop.auth;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthApplicationTests {
+
+    //@Test
+    void contextLoads() {
+    }
+
+}
