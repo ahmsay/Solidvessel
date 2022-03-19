1b8ff0c18672d0825a134ac5b1051ea972bbd3ad
ahmsay <ahmsay@gmail.com>
2022-03-19 12:14:49 +0300

    id generation strategy is specified. otherwise hibernate_sequence doesnt exist error is thrown

--- order/src/main/java/com/microshop/order/entity/CustomerOrder.java
+++ order/src/main/java/com/microshop/order/entity/CustomerOrder.java
@@ -2,6 +2,7 @@ package com.microshop.order.entity;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.validation.constraints.NotNull;
 
@@ -9,7 +10,7 @@ import javax.validation.constraints.NotNull;
 public class CustomerOrder {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @NotNull
--- payment/src/main/java/com/microshop/payment/entity/Payment.java
+++ payment/src/main/java/com/microshop/payment/entity/Payment.java
@@ -2,6 +2,7 @@ package com.microshop.payment.entity;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.validation.constraints.NotNull;
 
@@ -9,7 +10,7 @@ import javax.validation.constraints.NotNull;
 public class Payment {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @NotNull
