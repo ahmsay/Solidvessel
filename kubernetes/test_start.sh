466e078722d560f6b783d925dbc9bbdaa371ff6f
ahmsay <ahmsay@gmail.com>
2022-10-16 20:37:46 +0300

    dependencies are updated

--- kubernetes/test_start.sh
+++ kubernetes/test_start.sh
@@ -1,3 +1,3 @@
-kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.1.1/deploy/static/provider/cloud/deploy.yaml
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.4.0/deploy/static/provider/cloud/deploy.yaml
 kubectl create ns microshop
 kubectl apply -f ./kubernetes/test -R
