2bdc136e6c8d6bcee380ba7e531331f869562720
ahmsay <ahmsay@gmail.com>
2023-03-29 20:33:33 +0300

    image build scripts are moved to aws codebuild

--- .ci/build_and_push_images.sh
+++
@@ -1,5 +0,0 @@
-mvn clean compile jib:build -f ./auth/pom.xml
-mvn clean compile jib:build -f ./account/pom.xml
-mvn clean compile jib:build -f ./inventory/pom.xml
-mvn clean compile jib:build -f ./order/pom.xml
-mvn clean compile jib:build -f ./payment/pom.xml
