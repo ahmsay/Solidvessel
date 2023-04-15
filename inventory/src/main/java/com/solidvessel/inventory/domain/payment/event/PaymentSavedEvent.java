edee8f3d764a6856c1cd8c51f0019a36d76272a1
ahmsay <ahmsay@gmail.com>
2023-04-15 14:59:24 +0300

    inventory service is not notified about payment anymore

--- inventory/src/main/java/com/solidvessel/inventory/domain/payment/event/PaymentSavedEvent.java
+++
@@ -1,9 +0,0 @@
-package com.solidvessel.inventory.domain.payment.event;
-
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-
-import java.util.List;
-
-@JsonIgnoreProperties(ignoreUnknown = true)
-public record PaymentSavedEvent(Long paymentId, List<Long> productIds) {
-}
