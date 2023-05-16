46aabb0e1b11bdb6d62a3b07069c1e5b01ee23be
ahmsay <ahmsay@gmail.com>
2023-05-16 11:53:34 +0300

    compilation error is fixed

--- inventory/src/main/java/com/solidvessel/inventory/infra/configuration/DatabaseConfig.java
+++ inventory/src/main/java/com/solidvessel/inventory/infra/configuration/DatabaseConfig.java
@@ -13,14 +13,14 @@ public class DatabaseConfig {
     @Bean
     InitializingBean seedDatabase(final ProductRepository productRepository) {
         return () -> {
-            productRepository.save(new ProductJpaEntity(null, "Clipper", 2.5D, ProductCategory.TOOL));
-            productRepository.save(new ProductJpaEntity(null, "Laptop", 3000D, ProductCategory.ELECTRONICS));
-            productRepository.save(new ProductJpaEntity(null, "Phone", 5D, ProductCategory.ELECTRONICS));
-            productRepository.save(new ProductJpaEntity(null, "Skirt", 20D, ProductCategory.CLOTHING));
-            productRepository.save(new ProductJpaEntity(null, "Pants", 500D, ProductCategory.CLOTHING));
-            productRepository.save(new ProductJpaEntity(null, "Apple", 499.99D, ProductCategory.ELECTRONICS));
-            productRepository.save(new ProductJpaEntity(null, "Ice Pick", 53D, ProductCategory.TOOL));
-            productRepository.save(new ProductJpaEntity(null, "Desk", 25D, ProductCategory.FURNITURE));
+            productRepository.save(new ProductJpaEntity(null, "Clipper", 2.5D, ProductCategory.TOOL, 1));
+            productRepository.save(new ProductJpaEntity(null, "Laptop", 3000D, ProductCategory.ELECTRONICS, 5));
+            productRepository.save(new ProductJpaEntity(null, "Phone", 5D, ProductCategory.ELECTRONICS, 6));
+            productRepository.save(new ProductJpaEntity(null, "Skirt", 20D, ProductCategory.CLOTHING, 3));
+            productRepository.save(new ProductJpaEntity(null, "Pants", 500D, ProductCategory.CLOTHING, 7));
+            productRepository.save(new ProductJpaEntity(null, "Apple", 499.99D, ProductCategory.ELECTRONICS, 4));
+            productRepository.save(new ProductJpaEntity(null, "Ice Pick", 53D, ProductCategory.TOOL, 12));
+            productRepository.save(new ProductJpaEntity(null, "Desk", 25D, ProductCategory.FURNITURE, 3));
         };
     }
 }
