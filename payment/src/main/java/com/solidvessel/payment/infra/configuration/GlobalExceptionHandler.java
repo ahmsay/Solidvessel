70bfab9ab28e50a574eaa9442e235ffa7e245342
ahmsay <ahmsay@gmail.com>
2023-08-13 14:35:24 +0300

    global exception handling is added in payment service

---
+++ payment/src/main/java/com/solidvessel/payment/infra/configuration/GlobalExceptionHandler.java
@@ -0,0 +1,26 @@
+package com.solidvessel.payment.infra.configuration;
+
+import com.solidvessel.shared.domain.service.OperationResult;
+import com.solidvessel.shared.domain.service.ResultType;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(RuntimeException.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ResponseEntity<OperationResult> handleAllUncaughtException(
+            RuntimeException exception,
+            WebRequest request
+    ) {
+        return ResponseEntity
+                .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(new OperationResult(exception.getMessage(), ResultType.ERROR));
+    }
+}
