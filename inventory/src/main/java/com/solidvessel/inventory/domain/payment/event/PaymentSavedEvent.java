00026d7b0bcce715ec9ac806f9c92e60c32eda35
ahmsay <ahmsay@gmail.com>
2023-09-05 22:02:42 +0300

    ignoring unknown properties so events continue working

--- inventory/src/main/java/com/solidvessel/inventory/domain/payment/event/PaymentSavedEvent.java
+++ inventory/src/main/java/com/solidvessel/inventory/domain/payment/event/PaymentSavedEvent.java
@@ -1,9 +1,11 @@
 package com.solidvessel.inventory.domain.payment.event;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.solidvessel.inventory.domain.product.service.command.UpdateProductQuantitiesCommand;
 
 import java.util.Map;
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 public record PaymentSavedEvent(Map<Long, Integer> productQuantities) {
 
     public UpdateProductQuantitiesCommand toCommand() {
--- order/src/main/java/com/solidvessel/order/domain/payment/event/PaymentSavedEvent.java
+++ order/src/main/java/com/solidvessel/order/domain/payment/event/PaymentSavedEvent.java
@@ -1,7 +1,9 @@
 package com.solidvessel.order.domain.payment.event;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.solidvessel.order.domain.order.service.command.AddOrderCommand;
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 public record PaymentSavedEvent(Long paymentId, Long customerId) {
 
     public AddOrderCommand toCommand() {
