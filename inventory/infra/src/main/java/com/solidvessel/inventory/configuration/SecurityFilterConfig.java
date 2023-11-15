2428ddbe0d0c8ad334d93d37a47d8ebff5b8ca70
ahmsay <ahmsay@gmail.com>
2023-11-15 20:25:42 +0300

    refactor

--- inventory/infra/src/main/java/com/solidvessel/inventory/configuration/SecurityFilterConfig.java
+++ inventory/infra/src/main/java/com/solidvessel/inventory/configuration/SecurityFilterConfig.java
@@ -5,11 +5,9 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
-import static org.springframework.security.web.util.matcher.AntPathRequestMatcher.antMatcher;
 
 @Configuration
 @EnableWebSecurity
