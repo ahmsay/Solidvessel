dcf61873f0019890d780d151760cd3e389793c66
ahmsay <ahmsay@gmail.com>
2023-11-15 20:34:37 +0300

    payment service is configured as oauth2 resource server

--- payment/infra/src/main/java/com/solidvessel/payment/configuration/SecurityFilterConfig.java
+++ payment/infra/src/main/java/com/solidvessel/payment/configuration/SecurityFilterConfig.java
@@ -2,23 +2,23 @@ package com.solidvessel.payment.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
-import static org.springframework.security.web.util.matcher.AntPathRequestMatcher.antMatcher;
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
 public class SecurityFilterConfig {
 
     @Bean
     public SecurityFilterChain configure(final HttpSecurity httpSecurity) throws Exception {
         return httpSecurity.authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers(antMatcher("/ping")).permitAll()
                         .anyRequest().authenticated())
-                .logout(AbstractHttpConfigurer::disable)
-                .formLogin(AbstractHttpConfigurer::disable)
-                .csrf(AbstractHttpConfigurer::disable)
+                .oauth2ResourceServer(oauth2 -> oauth2.jwt(withDefaults()))
                 .build();
     }
 }
