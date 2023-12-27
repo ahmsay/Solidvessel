a06f6c7103bc82fefe341a1d3f7d5811b9d02052
ahmsay <ahmsay@gmail.com>
2023-12-27 19:33:55 +0300

    base database tests are added and postgres containers are set up in microservices

---
+++ payment/infra/src/test/java/com/solidvessel/payment/integrationtest/BaseDatabaseTest.java
@@ -0,0 +1,38 @@
+package com.solidvessel.payment.integrationtest;
+
+import com.solidvessel.shared.Profiles;
+import org.junit.jupiter.api.BeforeAll;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.stereotype.Repository;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+
+import static org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace.NONE;
+
+@DataJpaTest(includeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, value = {Repository.class}))
+@AutoConfigureTestDatabase(replace = NONE)
+@ActiveProfiles(Profiles.INTEGRATION_TEST)
+public class BaseDatabaseTest {
+
+    public static final PostgreSQLContainer<?> postgresqlContainer = new PostgreSQLContainer<>("postgres:16.1")
+            .withDatabaseName("payment")
+            .withUsername("nihilus")
+            .withPassword("nihilus");
+
+    @BeforeAll
+    public static void initializeContainer() {
+        postgresqlContainer.start();
+    }
+
+    @DynamicPropertySource
+    public static void overrideProperties(DynamicPropertyRegistry dynamicPropertyRegistry) {
+        dynamicPropertyRegistry.add("spring.datasource.url", postgresqlContainer::getJdbcUrl);
+        dynamicPropertyRegistry.add("spring.datasource.username", postgresqlContainer::getUsername);
+        dynamicPropertyRegistry.add("spring.datasource.password", postgresqlContainer::getPassword);
+    }
+}
