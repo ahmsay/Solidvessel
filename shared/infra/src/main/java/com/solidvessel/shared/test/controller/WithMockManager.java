a9367223de3a32f00927f5220b4a885205146880
ahmsay <ahmsay@gmail.com>
2024-01-05 19:05:38 +0300

    mock users are added for authorization in controller tests

---
+++ shared/infra/src/main/java/com/solidvessel/shared/test/controller/WithMockManager.java
@@ -0,0 +1,11 @@
+package com.solidvessel.shared.test.controller;
+
+import org.springframework.security.test.context.support.WithMockUser;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME)
+@WithMockUser(authorities = {"CUSTOMER", "MANAGER"})
+public @interface WithMockManager {
+}
