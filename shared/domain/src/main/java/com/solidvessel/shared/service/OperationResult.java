1977e03f5fe9b77550fe1a1e505ad3ea949839f3
ahmsay <ahmsay@gmail.com>
2024-01-05 19:03:55 +0300

    defaultSuccessResult is added

--- shared/domain/src/main/java/com/solidvessel/shared/service/OperationResult.java
+++ shared/domain/src/main/java/com/solidvessel/shared/service/OperationResult.java
@@ -1,4 +1,8 @@
 package com.solidvessel.shared.service;
 
 public record OperationResult(String message, ResultType resultType) {
+
+    public static OperationResult defaultSuccessResult() {
+        return new OperationResult("Operation is successful.", ResultType.SUCCESS);
+    }
 }
