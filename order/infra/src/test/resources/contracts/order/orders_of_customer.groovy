9a64dd3227561bad7ca9f654fa0f0bd691e0341b
ahmsay <ahmsay@gmail.com>
2024-01-12 21:45:08 +0300

    contract is written for orders of customer

---
+++ order/infra/src/test/resources/contracts/order/orders_of_customer.groovy
@@ -0,0 +1,20 @@
+package contracts.order
+
+import org.springframework.cloud.contract.spec.Contract
+
+Contract.make {
+    description "orders of customer"
+
+    request {
+        url "/ofCustomer/123"
+        method GET()
+    }
+
+    response {
+        status OK()
+        headers {
+            contentType applicationJson()
+        }
+        body('[{"id":1,"status":"DELIVERED","customerId":"123","paymentId":5},{"id":2,"status":"ON_THE_WAY","customerId":"123","paymentId":6}]')
+    }
+}
