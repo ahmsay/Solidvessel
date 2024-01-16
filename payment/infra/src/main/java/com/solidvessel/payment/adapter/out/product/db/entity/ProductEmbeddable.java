d4aa508aa69199590d5c23f1246dcca399da9362
ahmsay <ahmsay@gmail.com>
2024-01-17 00:27:27 +0300

    payment service errors are fixed

--- payment/infra/src/main/java/com/solidvessel/payment/adapter/out/product/db/entity/ProductEmbeddable.java
+++ payment/infra/src/main/java/com/solidvessel/payment/adapter/out/product/db/entity/ProductEmbeddable.java
@@ -3,6 +3,8 @@ package com.solidvessel.payment.adapter.out.product.db.entity;
 import com.solidvessel.payment.product.model.Product;
 import com.solidvessel.payment.product.model.ProductCategory;
 import jakarta.persistence.Embeddable;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -16,6 +18,7 @@ public class ProductEmbeddable {
     private Long productId;
     private String name;
     private Double price;
+    @Enumerated(EnumType.STRING)
     private ProductCategory category;
     private int quantity;
 
