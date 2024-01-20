3b0b16629594ad7aeebee5d1a2f9873e2e8e5233
ahmsay <ahmsay@gmail.com>
2024-01-20 15:58:29 +0300

    root controller tests are written

--- account/infra/src/main/java/com/solidvessel/account/adapter/in/ping/rest/RootController.java
+++ account/infra/src/main/java/com/solidvessel/account/adapter/in/ping/rest/RootController.java
@@ -1,5 +1,6 @@
 package com.solidvessel.account.adapter.in.ping.rest;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,14 +10,10 @@ import java.net.UnknownHostException;
 @RestController
 public class RootController {
 
+    @PreAuthorize("hasAuthority('CUSTOMER')")
     @GetMapping("/ping")
-    public String ping() {
-        try {
-            InetAddress address = InetAddress.getLocalHost();
-            return "Account microservice works. Host: %s".formatted(address.getHostName());
-        } catch (UnknownHostException e) {
-            e.printStackTrace();
-        }
-        return "Unknown host.";
+    public String ping() throws UnknownHostException {
+        InetAddress address = InetAddress.getLocalHost();
+        return "Account microservice works. Host: %s".formatted(address.getHostName());
     }
 }
---
+++ inventory/infra/src/test/java/com/solidvessel/inventory/adapter/in/ping/RootControllerTest.java
@@ -0,0 +1,33 @@
+package com.solidvessel.inventory.adapter.in.ping;
+
+import com.solidvessel.inventory.adapter.in.ping.rest.RootController;
+import com.solidvessel.shared.test.controller.BaseControllerTest;
+import com.solidvessel.shared.test.controller.WithMockCustomer;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+
+import java.net.InetAddress;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(controllers = {RootController.class})
+public class RootControllerTest extends BaseControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    @WithMockCustomer
+    void ping() throws Exception {
+        MvcResult mvcResult = mockMvc.perform(
+                get("/ping")
+        ).andExpect(status().isOk()).andReturn();
+        String host = InetAddress.getLocalHost().getHostName();
+        assertEquals("Inventory microservice works. Host: %s".formatted(host), bodyOf(mvcResult));
+    }
+}
