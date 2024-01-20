46876b5555239dd8bbf4375561fe8b5e91789894
ahmsay <ahmsay@gmail.com>
2024-01-20 15:42:19 +0300

    order domain exception is removed since it's not used. it can be added again when it's needed

--- order/domain/src/main/java/com/solidvessel/order/common/exception/OrderDomainException.java
+++
@@ -1,8 +0,0 @@
-package com.solidvessel.order.common.exception;
-
-public class OrderDomainException extends RuntimeException {
-
-    public OrderDomainException(String errorMessage) {
-        super(errorMessage);
-    }
-}
