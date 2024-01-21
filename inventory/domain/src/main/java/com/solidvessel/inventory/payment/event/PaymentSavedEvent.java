53970021114963e3d89a9211dc35c336dbb246b4
ahmsay <ahmsay@gmail.com>
2024-01-21 13:45:55 +0300

    compile error fix

--- inventory/domain/src/main/java/com/solidvessel/inventory/payment/event/PaymentSavedEvent.java
+++ inventory/domain/src/main/java/com/solidvessel/inventory/payment/event/PaymentSavedEvent.java
@@ -1,12 +1,6 @@
 package com.solidvessel.inventory.payment.event;
 
-import com.solidvessel.inventory.product.service.command.UpdateProductQuantitiesCommand;
-
 import java.util.Map;
 
 public record PaymentSavedEvent(Long paymentId, String customerId, Map<Long, Integer> productQuantities) {
-
-    public UpdateProductQuantitiesCommand toCommand() {
-        return new UpdateProductQuantitiesCommand(paymentId, productQuantities, customerId);
-    }
 }
