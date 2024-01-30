0fc05ab49d3ebcdca649fffac4602a5e7dd1ef17
ahmsay <ahmsay@gmail.com>
2024-01-30 22:15:24 +0300

    base entity is created

---
+++ shared/infra/src/main/java/com/solidvessel/shared/persistence/BaseEntity.java
@@ -0,0 +1,34 @@
+package com.solidvessel.shared.persistence;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.annotation.Version;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+@Getter
+@SuperBuilder
+@MappedSuperclass
+@NoArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
+public class BaseEntity implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @CreatedDate
+    private LocalDateTime createdDate;
+
+    @LastModifiedDate
+    private LocalDateTime lastModifiedDate;
+
+    @Version
+    private Integer version;
+}
