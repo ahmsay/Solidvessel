7a1434ad1826ba3d792d2b1263ff1c552fce191f
ahmsay <ahmsay@gmail.com>
2024-03-26 20:09:45 +0300

    cache config is set at account service

---
+++ account/infra/src/main/java/com/solidvessel/account/configuration/RedisCacheConfig.java
@@ -0,0 +1,20 @@
+package com.solidvessel.account.configuration;
+
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+
+import java.time.Duration;
+
+@EnableCaching
+@Configuration
+public class RedisCacheConfig {
+
+    @Bean
+    public RedisCacheConfiguration cacheConfiguration() {
+        return RedisCacheConfiguration.defaultCacheConfig()
+                .entryTtl(Duration.ofMinutes(60))
+                .disableCachingNullValues();
+    }
+}
