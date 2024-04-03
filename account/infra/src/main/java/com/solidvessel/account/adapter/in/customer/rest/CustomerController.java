d9fcd4f7ab256cbed38d4d4b9ed831850ef21bbd
ahmsay <ahmsay@gmail.com>
2024-04-03 21:58:07 +0300

    customers are cached

--- account/infra/src/main/java/com/solidvessel/account/adapter/in/customer/rest/CustomerController.java
+++ account/infra/src/main/java/com/solidvessel/account/adapter/in/customer/rest/CustomerController.java
@@ -10,6 +10,7 @@ import com.solidvessel.shared.security.SessionUtil;
 import lombok.RequiredArgsConstructor;
 import org.keycloak.admin.client.resource.RealmResource;
 import org.keycloak.representations.idm.UserRepresentation;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
@@ -24,6 +25,7 @@ public class CustomerController {
     private final OrderRestClient orderRestClient;
     private final PaymentRestClient paymentRestClient;
 
+    @Cacheable(value = "customers")
     @PreAuthorize("hasAuthority('MANAGER')")
     @GetMapping()
     public List<CustomerResponse> getUsers(@RequestParam Integer start, @RequestParam(required = false) Integer end) {
@@ -31,12 +33,14 @@ public class CustomerController {
         return users.stream().map(CustomerResponse::from).toList();
     }
 
+    @Cacheable(value = "customer", key = "id")
     @PreAuthorize("hasAuthority('MANAGER')")
     @GetMapping("/{id}")
     public CustomerResponse getById(@PathVariable final String id) {
         return CustomerResponse.from(keycloakRealm.users().get(id).toRepresentation());
     }
 
+    @Cacheable(value = "customerDetail", key = "id")
     @PreAuthorize("hasAuthority('MANAGER')")
     @GetMapping("/{id}/detail")
     public CustomerDetailResponse getDetailById(@PathVariable final String id) {
