8e7794cbd73aafe07ed957c7e0f096e29e5834d6
ahmsay <ahmsay@gmail.com>
2024-04-21 15:07:42 +0300

    order status is added to order in account service

---
+++ account/domain/src/main/java/com/solidvessel/account/order/model/OrderStatus.java
@@ -0,0 +1,8 @@
+package com.solidvessel.account.order.model;
+
+public enum OrderStatus {
+
+    PREPARING,
+    ON_THE_WAY,
+    DELIVERED
+}
--- account/infra/src/main/java/com/solidvessel/account/adapter/out/order/rest/response/OrderResponse.java
+++ account/infra/src/main/java/com/solidvessel/account/adapter/out/order/rest/response/OrderResponse.java
@@ -1,6 +1,8 @@
 package com.solidvessel.account.adapter.out.order.rest.response;
 
+import com.solidvessel.account.order.model.OrderStatus;
+
 import java.io.Serializable;
 
-public record OrderResponse(Long id, String status, Long paymentId) implements Serializable {
+public record OrderResponse(Long id, OrderStatus status, Long paymentId) implements Serializable {
 }
