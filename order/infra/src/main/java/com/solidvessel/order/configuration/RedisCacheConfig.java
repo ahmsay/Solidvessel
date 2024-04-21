4b1d7ebbe841051d6255fed65bf8a59147d709b4
ahmsay <ahmsay@gmail.com>
2024-04-21 14:22:43 +0300

    order object will use binary serialization

--- order/infra/src/main/java/com/solidvessel/order/configuration/RedisCacheConfig.java
+++ order/infra/src/main/java/com/solidvessel/order/configuration/RedisCacheConfig.java
@@ -5,8 +5,6 @@ import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
-import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
-import org.springframework.data.redis.serializer.RedisSerializationContext;
 
 import java.time.Duration;
 
@@ -27,7 +25,6 @@ public class RedisCacheConfig {
                 .withCacheConfiguration("order",
                         RedisCacheConfiguration.defaultCacheConfig()
                                 .entryTtl(Duration.ofMinutes(15))
-                                .disableCachingNullValues()
-                                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(new GenericJackson2JsonRedisSerializer())));
+                                .disableCachingNullValues());
     }
 }
