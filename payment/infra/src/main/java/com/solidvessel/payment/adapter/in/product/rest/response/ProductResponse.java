4d5cd3f7d27896c33a090e0fe83f804a7141e97c
ahmsay <ahmsay@gmail.com>
2024-04-21 14:09:12 +0300

    productresponse serialization fix

--- payment/infra/src/main/java/com/solidvessel/payment/adapter/in/product/rest/response/ProductResponse.java
+++ payment/infra/src/main/java/com/solidvessel/payment/adapter/in/product/rest/response/ProductResponse.java
@@ -3,7 +3,10 @@ package com.solidvessel.payment.adapter.in.product.rest.response;
 import com.solidvessel.payment.product.model.Product;
 import com.solidvessel.payment.product.model.ProductCategory;
 
-public record ProductResponse(Long id, String name, Double price, ProductCategory category, int quantity) {
+import java.io.Serializable;
+
+public record ProductResponse(Long id, String name, Double price, ProductCategory category,
+                              int quantity) implements Serializable {
 
     public static ProductResponse from(Product product) {
         return new ProductResponse(product.getId(), product.getName(), product.getPrice(), product.getCategory(), product.getQuantity());
