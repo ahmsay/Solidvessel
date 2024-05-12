d064daf73b1e8f68fc02cf0df7c0203bb8b3e680
ahmsay <ahmsay@gmail.com>
2024-05-12 16:44:17 +0300

    update payment db adapter test is added

--- payment/infra/src/test/java/com/solidvessel/payment/adapter/out/payment/db/PaymentDBAdapterTest.java
+++ payment/infra/src/test/java/com/solidvessel/payment/adapter/out/payment/db/PaymentDBAdapterTest.java
@@ -1,13 +1,18 @@
 package com.solidvessel.payment.adapter.out.payment.db;
 
+import com.solidvessel.payment.adapter.out.payment.db.entity.PaymentJpaEntity;
+import com.solidvessel.payment.adapter.out.product.db.entity.ProductEmbeddable;
 import com.solidvessel.payment.cart.model.Cart;
 import com.solidvessel.payment.integrationtest.BaseDatabaseTest;
 import com.solidvessel.payment.payment.model.Payment;
+import com.solidvessel.payment.payment.model.PaymentStatus;
 import com.solidvessel.payment.product.model.Product;
 import com.solidvessel.payment.product.model.ProductCategory;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 public class PaymentDBAdapterTest extends BaseDatabaseTest {
@@ -21,5 +26,22 @@ public class PaymentDBAdapterTest extends BaseDatabaseTest {
         var cart = new Cart("123", products);
         var payment = Payment.newPayment("123", cart);
         paymentDBAdapter.create(payment);
+    }
+
+    @Test
+    public void updatePayment() {
+        var productEmbeddable = new ProductEmbeddable(1L, "phone", 500D, ProductCategory.ELECTRONICS, 2);
+        List<ProductEmbeddable> products = new ArrayList<>();
+        products.add(productEmbeddable);
+        var paymentJpaEntity = PaymentJpaEntity.builder()
+                .customerId("123")
+                .products(products)
+                .totalPrice(1000D)
+                .status(PaymentStatus.PENDING)
+                .build();
+        var paymentJpaEntityFromDb = persistEntity(paymentJpaEntity);
+        var paymentFromDb = paymentJpaEntityFromDb.toDomainModel();
+        paymentFromDb.approve();
+        paymentDBAdapter.update(paymentFromDb);
     }
 }
