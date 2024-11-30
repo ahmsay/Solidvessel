e5d09e24b73f289c3494c1ea84418b3d31bd8b32
ahmsay <ahmsay@gmail.com>
2024-11-30 14:53:52 +0300

    product availability is created

---
+++ inventory/domain/src/main/java/com/solidvessel/inventory/product/model/ProductAvailability.java
@@ -0,0 +1,24 @@
+package com.solidvessel.inventory.product.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class ProductAvailability {
+
+    private Boolean isAvailable;
+    private UnavailableReason unavailableReason;
+
+    public static ProductAvailability available() {
+        return new ProductAvailability(true, null);
+    }
+
+    public static ProductAvailability notAvailableInRegion() {
+        return new ProductAvailability(false, UnavailableReason.NOT_AVAILABLE_IN_REGION);
+    }
+
+    public static ProductAvailability notInStocks() {
+        return new ProductAvailability(false, UnavailableReason.NOT_AVAILABLE_IN_REGION);
+    }
+}
---
+++ inventory/domain/src/main/java/com/solidvessel/inventory/product/model/UnavailableReason.java
@@ -0,0 +1,13 @@
+package com.solidvessel.inventory.product.model;
+
+public enum UnavailableReason {
+
+    NOT_IN_STOCKS("The product is not available in stocks with your desired quantity."),
+    NOT_AVAILABLE_IN_REGION("The product is not available in your region.");
+
+    public final String reason;
+
+    UnavailableReason(String reason) {
+        this.reason = reason;
+    }
+}
