7e21bdcca698ef102d99f4976f2768f70d42022c
ahmsay <ahmsay@gmail.com>
2024-12-04 23:52:31 +0300

    refactor

--- inventory/infra/src/main/java/com/solidvessel/inventory/adapter/out/product/db/repository/ProductRepository.java
+++ inventory/infra/src/main/java/com/solidvessel/inventory/adapter/out/product/db/repository/ProductRepository.java
@@ -4,9 +4,5 @@ import com.solidvessel.inventory.adapter.out.product.db.entity.ProductJpaEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
-import java.util.Optional;
-
 public interface ProductRepository extends JpaRepository<ProductJpaEntity, Long>, PagingAndSortingRepository<ProductJpaEntity, Long> {
-
-    Optional<ProductJpaEntity> findByIdAndQuantityGreaterThanEqual(Long id, Integer quantity);
 }
