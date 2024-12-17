22f300b53937c9f2b8a418a12c5fdf83a65ceafd
ahmsay <ahmsay@gmail.com>
2024-12-17 23:23:33 +0300

    tests are updated

--- account/domain/src/test/java/com/solidvessel/account/address/service/AddAddressCommandServiceTest.java
+++ account/domain/src/test/java/com/solidvessel/account/address/service/AddAddressCommandServiceTest.java
@@ -1,10 +1,12 @@
 package com.solidvessel.account.address.service;
 
+import com.solidvessel.account.address.event.PrimaryAddressSavedEvent;
 import com.solidvessel.account.address.model.Address;
 import com.solidvessel.account.address.port.AddressPort;
 import com.solidvessel.account.address.port.AddressQueryPort;
 import com.solidvessel.account.address.service.command.AddAddressCommand;
 import com.solidvessel.account.common.exception.AccountDomainException;
+import com.solidvessel.shared.event.EventPublisher;
 import com.solidvessel.shared.test.BaseUnitTest;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
@@ -21,32 +23,38 @@ public class AddAddressCommandServiceTest extends BaseUnitTest {
     @Mock
     private AddressQueryPort addressQueryPort;
 
+    @Mock
+    private EventPublisher<PrimaryAddressSavedEvent> primaryAddressSavedEventPublisher;
+
     @Test
     void addFirstAddress() {
         var command = new AddAddressCommand("home", "norway", "oslo", "245", "123");
-        var commandService = new AddAddressCommandService(addressPort, addressQueryPort);
+        var commandService = new AddAddressCommandService(addressPort, addressQueryPort, primaryAddressSavedEventPublisher);
         when(addressQueryPort.isAddressRegistered(command.customerId(), command.name())).thenReturn(false);
         when(addressQueryPort.getAddressCount(command.customerId())).thenReturn(0);
         commandService.execute(command);
         verify(addressPort).save(any(Address.class));
+        verify(primaryAddressSavedEventPublisher).publish(any(PrimaryAddressSavedEvent.class));
     }
 
     @Test
     void addNonFirstAddress() {
         var command = new AddAddressCommand("home", "norway", "oslo", "245", "123");
-        var commandService = new AddAddressCommandService(addressPort, addressQueryPort);
+        var commandService = new AddAddressCommandService(addressPort, addressQueryPort, primaryAddressSavedEventPublisher);
         when(addressQueryPort.isAddressRegistered(command.customerId(), command.name())).thenReturn(false);
         when(addressQueryPort.getAddressCount(command.customerId())).thenReturn(1);
         commandService.execute(command);
         verify(addressPort).save(any(Address.class));
+        verifyNoInteractions(primaryAddressSavedEventPublisher);
     }
 
     @Test
     void addressIsAlreadyRegistered() {
         var command = new AddAddressCommand("home", "norway", "oslo", "245", "123");
-        var commandService = new AddAddressCommandService(addressPort, addressQueryPort);
+        var commandService = new AddAddressCommandService(addressPort, addressQueryPort, primaryAddressSavedEventPublisher);
         when(addressQueryPort.isAddressRegistered(command.customerId(), command.name())).thenReturn(true);
         assertThrows(AccountDomainException.class, () -> commandService.execute(command));
         verifyNoInteractions(addressPort);
+        verifyNoInteractions(primaryAddressSavedEventPublisher);
     }
 }
--- account/domain/src/test/java/com/solidvessel/account/address/service/SetPrimaryAddressCommandServiceTest.java
+++ account/domain/src/test/java/com/solidvessel/account/address/service/SetPrimaryAddressCommandServiceTest.java
@@ -1,9 +1,11 @@
 package com.solidvessel.account.address.service;
 
+import com.solidvessel.account.address.event.PrimaryAddressSavedEvent;
 import com.solidvessel.account.address.model.Address;
 import com.solidvessel.account.address.port.AddressPort;
 import com.solidvessel.account.address.port.AddressQueryPort;
 import com.solidvessel.account.address.service.command.SetPrimaryAddressCommand;
+import com.solidvessel.shared.event.EventPublisher;
 import com.solidvessel.shared.service.ResultType;
 import com.solidvessel.shared.test.BaseUnitTest;
 import org.junit.jupiter.api.Test;
@@ -21,24 +23,29 @@ public class SetPrimaryAddressCommandServiceTest extends BaseUnitTest {
     @Mock
     private AddressQueryPort addressQueryPort;
 
+    @Mock
+    private EventPublisher<PrimaryAddressSavedEvent> primaryAddressSavedEventPublisher;
+
     @Test
     void setPrimaryAddress() {
         var command = new SetPrimaryAddressCommand(1L, "123");
-        var commandService = new SetPrimaryAddressCommandService(addressQueryPort, addressPort);
+        var commandService = new SetPrimaryAddressCommandService(addressQueryPort, addressPort, primaryAddressSavedEventPublisher);
         when(addressQueryPort.getPrimaryAddress("123")).thenReturn(Address.builder().id(2L).isPrimary(true).build());
         when(addressQueryPort.getByIdAndCustomerId(1L, "123")).thenReturn(Address.builder().id(1L).isPrimary(false).build());
         var result = commandService.execute(command);
         verify(addressPort, times(2)).save(any(Address.class));
+        verify(primaryAddressSavedEventPublisher).publish(any(PrimaryAddressSavedEvent.class));
         assertEquals(ResultType.SUCCESS, result.resultType());
     }
 
     @Test
     void addressIsAlreadyPrimary() {
         var command = new SetPrimaryAddressCommand(1L, "123");
-        var commandService = new SetPrimaryAddressCommandService(addressQueryPort, addressPort);
+        var commandService = new SetPrimaryAddressCommandService(addressQueryPort, addressPort, primaryAddressSavedEventPublisher);
         when(addressQueryPort.getPrimaryAddress("123")).thenReturn(Address.builder().id(1L).isPrimary(true).build());
         var result = commandService.execute(command);
         verifyNoInteractions(addressPort);
         assertEquals(ResultType.ERROR, result.resultType());
+        verifyNoInteractions(primaryAddressSavedEventPublisher);
     }
 }
