ef38ec5f00510268d64e8396b1a1fba96fd091de
ahmsay <ahmsay@gmail.com>
2024-12-24 21:56:41 +0300

    missing bean added

--- account/domain/src/test/java/com/solidvessel/account/address/service/UpdateAddressCommandServiceTest.java
+++ account/domain/src/test/java/com/solidvessel/account/address/service/UpdateAddressCommandServiceTest.java
@@ -1,10 +1,12 @@
 package com.solidvessel.account.address.service;
 
+import com.solidvessel.account.address.event.PrimaryAddressSavedEvent;
 import com.solidvessel.account.address.model.Address;
 import com.solidvessel.account.address.port.AddressPort;
 import com.solidvessel.account.address.port.AddressQueryPort;
 import com.solidvessel.account.address.service.command.UpdateAddressCommand;
 import com.solidvessel.account.common.exception.AccountDomainException;
+import com.solidvessel.shared.event.EventPublisher;
 import com.solidvessel.shared.test.BaseUnitTest;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
@@ -21,12 +23,15 @@ public class UpdateAddressCommandServiceTest extends BaseUnitTest {
     @Mock
     private AddressQueryPort addressQueryPort;
 
+    @Mock
+    private EventPublisher<PrimaryAddressSavedEvent> primaryAddressSavedEventPublisher;
+
     @Test
     void updateAddress() {
         var command = new UpdateAddressCommand(1L, "home", "norway", "oslo", "245", "123");
-        var commandService = new UpdateAddressCommandService(addressPort, addressQueryPort);
+        var commandService = new UpdateAddressCommandService(addressPort, addressQueryPort, primaryAddressSavedEventPublisher);
         when(addressQueryPort.isAddressRegistered(command.customerId(), command.name())).thenReturn(true);
-        when(addressQueryPort.getByIdAndCustomerId(1L, "123")).thenReturn(Address.builder().id(1L).customerId("123").name("home").country("turkey").city("eskisehir").zipCode("26200").build());
+        when(addressQueryPort.getByIdAndCustomerId(1L, "123")).thenReturn(Address.builder().id(1L).customerId("123").name("home").country("turkey").city("eskisehir").zipCode("26200").isPrimary(true).build());
         commandService.execute(command);
         verify(addressPort).save(any(Address.class));
     }
@@ -34,7 +39,7 @@ public class UpdateAddressCommandServiceTest extends BaseUnitTest {
     @Test
     void addressIsAlreadyRegistered() {
         var command = new UpdateAddressCommand(1L, "home", "norway", "oslo", "245", "123");
-        var commandService = new UpdateAddressCommandService(addressPort, addressQueryPort);
+        var commandService = new UpdateAddressCommandService(addressPort, addressQueryPort, primaryAddressSavedEventPublisher);
         when(addressQueryPort.isAddressRegistered(command.customerId(), command.name())).thenReturn(false);
         assertThrows(AccountDomainException.class, () -> commandService.execute(command));
         verifyNoInteractions(addressPort);
