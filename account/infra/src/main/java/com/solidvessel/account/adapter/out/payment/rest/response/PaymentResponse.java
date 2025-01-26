1c22d4412728d69abc92ce4843227a64a8bc077a
ahmsay <ahmsay@gmail.com>
2025-01-26 14:11:24 +0300

    payment status and creation date is added to payment response

--- account/infra/src/main/java/com/solidvessel/account/adapter/out/payment/rest/response/PaymentResponse.java
+++ account/infra/src/main/java/com/solidvessel/account/adapter/out/payment/rest/response/PaymentResponse.java
@@ -1,6 +1,8 @@
 package com.solidvessel.account.adapter.out.payment.rest.response;
 
 import java.io.Serializable;
+import java.time.LocalDateTime;
 
-public record PaymentResponse(Long id, Double totalCharge) implements Serializable {
+public record PaymentResponse(Long id, Double totalCharge, PaymentStatus status, LocalDateTime creationDate
+) implements Serializable {
 }
---
+++ account/infra/src/main/java/com/solidvessel/account/adapter/out/payment/rest/response/PaymentStatus.java
@@ -0,0 +1,8 @@
+package com.solidvessel.account.adapter.out.payment.rest.response;
+
+public enum PaymentStatus {
+
+    PENDING,
+    APPROVED,
+    CANCELLED
+}
