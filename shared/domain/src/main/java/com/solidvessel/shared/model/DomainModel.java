746ddefbdcb0486eae5fc8b08b52b8dd8cc1f1ed
ahmsay <ahmsay@gmail.com>
2025-02-16 18:55:46 +0300

    domain model's equals & hashcode is overriden by using the new approach

--- shared/domain/src/main/java/com/solidvessel/shared/model/DomainModel.java
+++ shared/domain/src/main/java/com/solidvessel/shared/model/DomainModel.java
@@ -6,9 +6,7 @@ import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
 
 import java.io.Serializable;
-import java.lang.reflect.Field;
 import java.time.LocalDateTime;
-import java.util.Objects;
 
 @Getter
 @SuperBuilder
@@ -23,57 +21,12 @@ public class DomainModel implements Serializable {
     @Generated
     @Override
     public boolean equals(Object obj) {
-        if (this == obj) return true;
-        if (obj == null || getClass() != obj.getClass()) return false;
-
-        try {
-            for (Field field : getAllFields(this.getClass())) {
-                field.setAccessible(true);
-                Object thisValue = field.get(this);
-                Object otherValue = field.get(obj);
-
-                if (!Objects.equals(thisValue, otherValue)) {
-                    return false;
-                }
-            }
-            return true;
-        } catch (IllegalAccessException e) {
-            throw new RuntimeException("Error accessing fields in equals()", e);
-        }
+        return Equalizer.equals(this, obj);
     }
 
     @Generated
     @Override
     public int hashCode() {
-        try {
-            Object[] fieldValues = new Object[getAllFields(this.getClass()).length];
-            int index = 0;
-
-            for (Field field : getAllFields(this.getClass())) {
-                field.setAccessible(true);
-                fieldValues[index++] = field.get(this);
-            }
-
-            return Objects.hash(fieldValues);
-        } catch (IllegalAccessException e) {
-            throw new RuntimeException("Error accessing fields in hashCode()", e);
-        }
-    }
-
-    @Generated
-    private static Field[] getAllFields(Class<?> clazz) {
-        Field[] declaredFields = clazz.getDeclaredFields();
-        Class<?> superclass = clazz.getSuperclass();
-
-        if (superclass != null && superclass != Object.class) {
-            Field[] parentFields = getAllFields(superclass);
-            Field[] allFields = new Field[declaredFields.length + parentFields.length];
-
-            System.arraycopy(declaredFields, 0, allFields, 0, declaredFields.length);
-            System.arraycopy(parentFields, 0, allFields, declaredFields.length, parentFields.length);
-
-            return allFields;
-        }
-        return declaredFields;
+        return Equalizer.hashCode(this);
     }
 }
