b6927131ad609e08bc4eb6b9a25a74fe1ac30fb5
ahmsay <ahmsay@gmail.com>
2025-02-16 18:49:14 +0300

    equalizer is created for manual equals hand hashcode

---
+++ shared/domain/src/main/java/com/solidvessel/shared/model/Equalizer.java
@@ -0,0 +1,67 @@
+package com.solidvessel.shared.model;
+
+import lombok.Generated;
+
+import java.lang.reflect.Field;
+import java.util.Objects;
+
+public final class Equalizer {
+
+    private Equalizer() {
+    }
+
+    @Generated
+    public static boolean equals(Object current, Object other) {
+        if (current == other) return true;
+        if (other == null || current.getClass() != other.getClass()) return false;
+
+        try {
+            for (Field field : getAllFields(current.getClass())) {
+                field.setAccessible(true);
+                Object thisValue = field.get(current);
+                Object otherValue = field.get(other);
+
+                if (!Objects.equals(thisValue, otherValue)) {
+                    return false;
+                }
+            }
+            return true;
+        } catch (IllegalAccessException e) {
+            throw new RuntimeException("Error accessing fields in equals()", e);
+        }
+    }
+
+    @Generated
+    public static int hashCode(Object obj) {
+        try {
+            Object[] fieldValues = new Object[getAllFields(obj.getClass()).length];
+            int index = 0;
+
+            for (Field field : getAllFields(obj.getClass())) {
+                field.setAccessible(true);
+                fieldValues[index++] = field.get(obj);
+            }
+
+            return Objects.hash(fieldValues);
+        } catch (IllegalAccessException e) {
+            throw new RuntimeException("Error accessing fields in hashCode()", e);
+        }
+    }
+
+    @Generated
+    private static Field[] getAllFields(Class<?> clazz) {
+        Field[] declaredFields = clazz.getDeclaredFields();
+        Class<?> superclass = clazz.getSuperclass();
+
+        if (superclass != null && superclass != Object.class) {
+            Field[] parentFields = getAllFields(superclass);
+            Field[] allFields = new Field[declaredFields.length + parentFields.length];
+
+            System.arraycopy(declaredFields, 0, allFields, 0, declaredFields.length);
+            System.arraycopy(parentFields, 0, allFields, declaredFields.length, parentFields.length);
+
+            return allFields;
+        }
+        return declaredFields;
+    }
+}
