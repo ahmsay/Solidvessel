e9a9be88c749a7f5822ac8a0a310254f413240c5
ahmsay <ahmsay@gmail.com>
2025-02-20 21:00:49 +0300

    customer id is added to the request

--- order/infra/src/main/java/com/solidvessel/order/adapter/in/order/rest/request/CancelOrderRequest.java
+++ order/infra/src/main/java/com/solidvessel/order/adapter/in/order/rest/request/CancelOrderRequest.java
@@ -2,10 +2,11 @@ package com.solidvessel.order.adapter.in.order.rest.request;
 
 import com.solidvessel.order.order.model.CancellationReason;
 import com.solidvessel.order.order.service.command.CancelOrderCommand;
+import com.solidvessel.shared.security.SessionUtil;
 
 public record CancelOrderRequest(CancellationReason reason, String explanation) {
 
     public CancelOrderCommand toCommand(Long id) {
-        return new CancelOrderCommand(id, reason, explanation);
+        return new CancelOrderCommand(id, SessionUtil.getCurrentUserId(), reason, explanation);
     }
 }
