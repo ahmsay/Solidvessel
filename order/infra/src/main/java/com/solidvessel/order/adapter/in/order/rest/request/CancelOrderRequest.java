3fe86a3e6be407a49cf9ee3cf30b053cf5246a38
ahmsay <ahmsay@gmail.com>
2025-02-20 21:14:33 +0300

    refactor

--- order/infra/src/main/java/com/solidvessel/order/adapter/in/order/rest/request/CancelOrderRequest.java
+++ order/infra/src/main/java/com/solidvessel/order/adapter/in/order/rest/request/CancelOrderRequest.java
@@ -4,9 +4,9 @@ import com.solidvessel.order.order.model.CancellationReason;
 import com.solidvessel.order.order.service.command.CancelOrderCommand;
 import com.solidvessel.shared.security.SessionUtil;
 
-public record CancelOrderRequest(CancellationReason reason, String explanation) {
+public record CancelOrderRequest(CancellationReason cancellationReason, String explanation) {
 
     public CancelOrderCommand toCommand(Long id) {
-        return new CancelOrderCommand(id, SessionUtil.getCurrentUserId(), reason, explanation);
+        return new CancelOrderCommand(id, SessionUtil.getCurrentUserId(), cancellationReason, explanation);
     }
 }
