83383e42ce502eff52a9d2ed6c292d60bf5cea58
ahmsay <ahmsay@gmail.com>
2025-02-22 15:46:02 +0300

    unused profiles are removed

--- shared/infra/src/main/java/com/solidvessel/shared/Profiles.java
+++ shared/infra/src/main/java/com/solidvessel/shared/Profiles.java
@@ -2,9 +2,6 @@ package com.solidvessel.shared;
 
 public interface Profiles {
 
-    String DEV = "dev";
-    String TEST = "test";
-    String PROD = "prod";
     String INTEGRATION_TEST = "integrationtest";
     String CONTRACT_TEST = "contracttest";
 }
