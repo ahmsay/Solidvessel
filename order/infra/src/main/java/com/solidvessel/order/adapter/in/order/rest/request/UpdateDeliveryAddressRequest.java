caec85d5008330538550e9732502d685ea85bf43
ahmsay <ahmsay@gmail.com>
2025-05-10 14:38:44 +0300

    customerId is added to the command

--- order/infra/src/main/java/com/solidvessel/order/adapter/in/order/rest/request/UpdateDeliveryAddressRequest.java
+++ order/infra/src/main/java/com/solidvessel/order/adapter/in/order/rest/request/UpdateDeliveryAddressRequest.java
@@ -1,10 +1,11 @@
 package com.solidvessel.order.adapter.in.order.rest.request;
 
 import com.solidvessel.order.order.service.command.UpdateDeliveryAddressCommand;
+import com.solidvessel.shared.security.SessionUtil;
 
 public record UpdateDeliveryAddressRequest(String address) {
 
     public UpdateDeliveryAddressCommand toCommand(Long id) {
-        return new UpdateDeliveryAddressCommand(id, address);
+        return new UpdateDeliveryAddressCommand(id, SessionUtil.getCurrentUserId(), address);
     }
 }
