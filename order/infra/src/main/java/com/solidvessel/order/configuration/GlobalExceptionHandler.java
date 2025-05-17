8d807ea83d004f66b0b8282522bef98ce4ab7444
ahmsay <ahmsay@gmail.com>
2025-05-17 15:06:05 +0300

    entitynotfound exception is handled in order service

---
+++ order/infra/src/main/java/com/solidvessel/order/configuration/GlobalExceptionHandler.java
@@ -0,0 +1,23 @@
+package com.solidvessel.order.configuration;
+
+import com.solidvessel.shared.service.OperationResult;
+import com.solidvessel.shared.service.ResultType;
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ResponseEntity<OperationResult> handleEntityNotFoundException(EntityNotFoundException exception) {
+        return ResponseEntity
+                .status(HttpStatus.NOT_FOUND)
+                .body(new OperationResult(exception.getMessage(), ResultType.ERROR));
+    }
+}
