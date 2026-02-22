3861404ff4c421689518a678a4b60b2402fca3e1
ahmsay <ahmsay@gmail.com>
2026-02-22 15:20:00 +0300

    order detail response is simplified

--- order/infra/src/main/java/com/solidvessel/order/adapter/in/order/rest/response/OrderDetailResponse.java
+++ order/infra/src/main/java/com/solidvessel/order/adapter/in/order/rest/response/OrderDetailResponse.java
@@ -2,12 +2,7 @@ package com.solidvessel.order.adapter.in.order.rest.response;
 
 import com.solidvessel.order.adapter.out.customer.rest.response.CustomerResponse;
 import com.solidvessel.order.adapter.out.payment.rest.response.PaymentResponse;
-import com.solidvessel.order.order.model.OrderCancellation;
-import com.solidvessel.order.order.model.OrderStatus;
 
-import java.time.LocalDateTime;
-
-public record OrderDetailResponse(Long id, OrderStatus status, String address, LocalDateTime createdDate,
-                                  OrderCancellation cancellation, String recipient, CustomerResponse customer,
+public record OrderDetailResponse(OrderResponse order, CustomerResponse customer,
                                   PaymentResponse payment) {
 }
