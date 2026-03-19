b8f3a4cbf112b703ebed1a42e8fd63dcfb9a19c0
ahmsay <ahmsay@gmail.com>
2026-03-19 21:16:58 +0300

    spring data jpa test package name change is applied

--- shared/infra/src/main/java/com/solidvessel/shared/test/database/TestEntityHelper.java
+++ shared/infra/src/main/java/com/solidvessel/shared/test/database/TestEntityHelper.java
@@ -1,7 +1,7 @@
 package com.solidvessel.shared.test.database;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.boot.jpa.test.autoconfigure.TestEntityManager;
 
 public class TestEntityHelper {
 
