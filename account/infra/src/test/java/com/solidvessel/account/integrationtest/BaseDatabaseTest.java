7da109fb8073ad57d01692516c8f54cc89708f20
ahmsay <ahmsay@gmail.com>
2026-04-06 22:44:48 +0300

    same test db config is applied to other microservices' base database test classes

--- account/infra/src/test/java/com/solidvessel/account/integrationtest/BaseDatabaseTest.java
+++ account/infra/src/test/java/com/solidvessel/account/integrationtest/BaseDatabaseTest.java
@@ -1,10 +1,11 @@
 package com.solidvessel.account.integrationtest;
 
 import com.solidvessel.shared.Profiles;
-import com.solidvessel.shared.test.database.TestEntityHelper;
 import org.junit.jupiter.api.BeforeAll;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.FilterType;
 import org.springframework.stereotype.Repository;
@@ -16,9 +17,17 @@ import org.testcontainers.containers.PostgreSQLContainer;
 import static org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace.NONE;
 
 @DataJpaTest(includeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, value = {Repository.class}))
+@ComponentScan(
+        basePackages = "com.solidvessel",
+        includeFilters = @ComponentScan.Filter(type = FilterType.REGEX, pattern = ".*JpaMapperImpl"),
+        useDefaultFilters = false
+)
 @AutoConfigureTestDatabase(replace = NONE)
 @ActiveProfiles(Profiles.INTEGRATION_TEST)
-public class BaseDatabaseTest extends TestEntityHelper {
+public class BaseDatabaseTest {
+
+    @Autowired
+    private TestEntityManager testEntityManager;
 
     public static final PostgreSQLContainer<?> postgresqlContainer = new PostgreSQLContainer<>("postgres:16.4")
             .withDatabaseName("account")
@@ -35,5 +44,13 @@ public class BaseDatabaseTest extends TestEntityHelper {
         dynamicPropertyRegistry.add("spring.datasource.url", postgresqlContainer::getJdbcUrl);
         dynamicPropertyRegistry.add("spring.datasource.username", postgresqlContainer::getUsername);
         dynamicPropertyRegistry.add("spring.datasource.password", postgresqlContainer::getPassword);
+    }
+
+    public <T> T persistEntity(final T entity) {
+        return testEntityManager.persist(entity);
+    }
+
+    public <T> T find(final Long id, final Class<T> entityClass) {
+        return testEntityManager.find(entityClass, id);
     }
 }
