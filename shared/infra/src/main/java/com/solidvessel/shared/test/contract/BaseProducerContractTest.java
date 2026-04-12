9933f84256ab496dde8907cb53b728053357b4cc
ahmsay <ahmsay@gmail.com>
2026-04-12 15:20:32 +0300

    contract tests should find and inject web mappers

--- shared/infra/src/main/java/com/solidvessel/shared/test/contract/BaseProducerContractTest.java
+++ shared/infra/src/main/java/com/solidvessel/shared/test/contract/BaseProducerContractTest.java
@@ -1,8 +1,15 @@
 package com.solidvessel.shared.test.contract;
 
 import com.solidvessel.shared.Profiles;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
 import org.springframework.test.context.ActiveProfiles;
 
 @ActiveProfiles(Profiles.CONTRACT_TEST)
+@ComponentScan(
+        basePackages = "com.solidvessel",
+        includeFilters = @ComponentScan.Filter(type = FilterType.REGEX, pattern = ".*WebMapperImpl"),
+        useDefaultFilters = false
+)
 public class BaseProducerContractTest {
 }
