6f6ae5d76822a31f4a320e26faefd500fc4496ea
ahmsay <ahmsay@gmail.com>
2026-04-23 17:01:09 +0300

    controller tests import update

--- order/infra/src/test/java/com/solidvessel/order/adapter/in/ping/rest/RootControllerTest.java
+++ order/infra/src/test/java/com/solidvessel/order/adapter/in/ping/rest/RootControllerTest.java
@@ -4,7 +4,7 @@ import com.solidvessel.shared.test.controller.BaseControllerTest;
 import com.solidvessel.shared.test.controller.WithMockCustomer;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 
