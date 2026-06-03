196b2a6680312555d590490e49bcfb13de4e6594
ahmsay <ahmsay@gmail.com>
2026-06-03 22:32:58 +0300

    redis cache change 2

--- shared/infra/src/main/java/com/solidvessel/shared/cache/RedisCacheConfigForUsers.java
+++ shared/infra/src/main/java/com/solidvessel/shared/cache/RedisCacheConfigForUsers.java
@@ -1,40 +1,34 @@
 package com.solidvessel.shared.cache;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.jsontype.BasicPolymorphicTypeValidator;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.keycloak.representations.idm.UserRepresentation;
 import org.springframework.boot.cache.autoconfigure.RedisCacheManagerBuilderCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
-import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.JacksonJsonRedisSerializer;
 import org.springframework.data.redis.serializer.RedisSerializationContext;
 
 import java.time.Duration;
+import java.util.List;
 
 @Configuration
 public class RedisCacheConfigForUsers {
 
     @Bean
     public RedisCacheManagerBuilderCustomizer redisCacheManagerBuilderCustomizerForUsers() {
-        ObjectMapper objectMapper = new ObjectMapper()
-                .registerModule(new JavaTimeModule())
-                .activateDefaultTyping(
-                        BasicPolymorphicTypeValidator.builder()
-                                .allowIfSubType("org.keycloak.representations")
-                                .allowIfSubType("java.util")
-                                .allowIfSubType("java.lang")
-                                .build(),
-                        ObjectMapper.DefaultTyping.NON_FINAL);
-        GenericJackson2JsonRedisSerializer serializer = new GenericJackson2JsonRedisSerializer(objectMapper);
+        ObjectMapper objectMapper = new ObjectMapper().registerModule(new JavaTimeModule());
         return (builder) -> builder
                 .withCacheConfiguration("users",
                         RedisCacheConfiguration.defaultCacheConfig()
                                 .entryTtl(Duration.ofMinutes(5))
-                                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(serializer)))
+                                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(
+                                        new JacksonJsonRedisSerializer<>(objectMapper, List.class))))
                 .withCacheConfiguration("user",
                         RedisCacheConfiguration.defaultCacheConfig()
                                 .entryTtl(Duration.ofMinutes(30))
-                                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(serializer)));
+                                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(
+                                        new JacksonJsonRedisSerializer<>(objectMapper, UserRepresentation.class))));
     }
 }
