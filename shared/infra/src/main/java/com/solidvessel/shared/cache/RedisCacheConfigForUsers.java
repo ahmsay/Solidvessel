cd0532b3c942a05c1f61257a6f4d8a792f3e3720
ahmsay <ahmsay@gmail.com>
2026-06-03 22:36:16 +0300

    Revert "redis cache change"
    This reverts commit d68bb3c18e9e11e21acffd9e364e97dc0e590b54.

--- shared/infra/src/main/java/com/solidvessel/shared/cache/RedisCacheConfigForUsers.java
+++ shared/infra/src/main/java/com/solidvessel/shared/cache/RedisCacheConfigForUsers.java
@@ -1,14 +1,9 @@
 package com.solidvessel.shared.cache;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.jsontype.BasicPolymorphicTypeValidator;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.boot.cache.autoconfigure.RedisCacheManagerBuilderCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
-import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
-import org.springframework.data.redis.serializer.RedisSerializationContext;
 
 import java.time.Duration;
 
@@ -17,24 +12,12 @@ public class RedisCacheConfigForUsers {
 
     @Bean
     public RedisCacheManagerBuilderCustomizer redisCacheManagerBuilderCustomizerForUsers() {
-        ObjectMapper objectMapper = new ObjectMapper()
-                .registerModule(new JavaTimeModule())
-                .activateDefaultTyping(
-                        BasicPolymorphicTypeValidator.builder()
-                                .allowIfSubType("org.keycloak.representations")
-                                .allowIfSubType("java.util")
-                                .allowIfSubType("java.lang")
-                                .build(),
-                        ObjectMapper.DefaultTyping.NON_FINAL);
-        GenericJackson2JsonRedisSerializer serializer = new GenericJackson2JsonRedisSerializer(objectMapper);
         return (builder) -> builder
                 .withCacheConfiguration("users",
                         RedisCacheConfiguration.defaultCacheConfig()
-                                .entryTtl(Duration.ofMinutes(5))
-                                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(serializer)))
+                                .entryTtl(Duration.ofMinutes(5)))
                 .withCacheConfiguration("user",
                         RedisCacheConfiguration.defaultCacheConfig()
-                                .entryTtl(Duration.ofMinutes(30))
-                                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(serializer)));
+                                .entryTtl(Duration.ofMinutes(30)));
     }
 }
