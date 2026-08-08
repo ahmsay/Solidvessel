822079cddefbd0eda68ac9cfa56b26f50dadfb03
ahmsay <ahmsay@gmail.com>
2026-08-08 20:21:13 +0300

    refactor

--- inventory/infra/src/main/java/com/solidvessel/inventory/adapter/out/product/db/mapper/ProductJpaMapper.java
+++ inventory/infra/src/main/java/com/solidvessel/inventory/adapter/out/product/db/mapper/ProductJpaMapper.java
@@ -4,12 +4,9 @@ import com.solidvessel.inventory.adapter.out.product.db.entity.ProductJpaEntity;
 import com.solidvessel.inventory.product.model.Product;
 import org.mapstruct.Mapper;
 import org.mapstruct.ReportingPolicy;
-import org.mapstruct.factory.Mappers;
 
 @Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.ERROR)
 public interface ProductJpaMapper {
-
-    ProductJpaMapper INSTANCE = Mappers.getMapper(ProductJpaMapper.class);
 
     ProductJpaEntity toJpaEntity(Product product);
 
